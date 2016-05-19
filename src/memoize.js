8f82eb6d670972a8defba5cade5da4d32a97c16b
Iuliia Savich <Iuliia_Savich@epam.com>
2016-05-19 10:18:05 +0300

    Implements memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,13 @@
 export { memoize };
 
 function memoize(func) {
-	return func; // TODO
+	var _cache = {};
+	return function () {
+	    var key = JSON.stringify(arguments);
+	    if (key in _cache) {
+	    	return _cache[key];
+	    }
+	    _cache[key] = func.apply(null, arguments);
+	    return _cache[key];
+  	};
 }
